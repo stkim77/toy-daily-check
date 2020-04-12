51e1442ce109bf53fa3f474259ed81a97050d629
stkim77 <stkim77@gmail.com>
2020-04-12 14:43:38 +0900

    add logic to move week

--- components/weeklyDisplay.tsx
+++ components/weeklyDisplay.tsx
@@ -1,92 +1,94 @@
 import React, { Component } from 'react';
 import * as R from 'ramda';
 import { Layout, Calendar, Badge, Button, List, Card, DatePicker, Row, Col } from 'antd';
+import ko_locale from 'antd/es/date-picker/locale/ko_KR';
 import moment from 'moment';
-import { disconnect } from 'cluster';
+import { WEEK_TITLE, weekTitleObj } from '../config/constant';
 
-const { Content, Sider } = Layout;
+moment.updateLocale("en", { week: {
+  dow: 1, // First day of week is Monday
+  doy: 4  // First week of year must contain 4 January (7 + 1 - 4)
+}});
 
-enum statusType {
-  success = 'success',
-  processing = 'processing',
-  default = 'default',
-  error = 'error',
-  warning = 'warning'
+interface titleObj {
+  title : string,
+  color : string,
+  date : string
 }
-
-type listType = {
-  type: statusType,
-  content: string,
-};
-
-const data = [
-  {
-    title: 'Monday',
-  },
-  {
-    title: 'Tuesday',
-  },
-  {
-    title: 'Wednesday',
-  },
-  {
-    title: 'Thursday',
-  },
-  {
-    title: 'Friday',
-  },
-  {
-    title: 'Saturday',
-  },
-  {
-    title: 'Sunday',
-  },
-];
 
 class WeeklyDisplay extends Component {
   state = {
     collapsed: false,
-    value: moment(),
+    selectedDate: moment(),
+    weekData: [],
     displayedYear: moment().format('YYYY'),
     displayedMonth: moment().format('MM')
   };
 
-  onCollapse = (collapsed: boolean) => {
-    this.setState({ collapsed });
-  };
-
   goToday = () : void => {
     this.setState({
-      value: moment()
+      selectedDate: moment()
     });
   }
 
   onChange = (date : moment.Moment | null, dateString : string) : void => {
-    console.log(date, dateString);
-    console.log(date?.format('YYYY-wo'));
-    console.log(date?.format('YYYY-MM-DD'));
-  }  
+    if (!R.isNil(date)) {
+      this.setState({
+        selectedDate: date
+      });
+      // console.log(date, dateString);
+      // console.log(date.format('YYYY-wo'));
+      // console.log(date.format('YYYY-MM-DD'));
+      // console.log(date.startOf('isoWeek').format('YYYY-MM-DD'));  
+    }
+  }
+
+  addDateToWeekTitle = () : titleObj[] => {
+    const { selectedDate } = this.state;
+    const monday = moment(selectedDate).startOf('week');
+    return WEEK_TITLE.map(
+      (item, index) => {
+        return { ...item,
+          date: moment(monday).add(index, 'day').format('YYYY-MM-DD')
+        };
+      }
+    );
+  };
+
+  makeTitleNode = (item : titleObj) : React.ReactNode => (
+    <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
+      <div>{item.title}</div>
+      <div style={{fontSize: '12px'}}>({item.date})</div>
+    </div>
+  );
   
   render() {
-    const { value } = this.state;
+    const { selectedDate } = this.state;
+    const weekData = this.addDateToWeekTitle();
     return (
       <React.Fragment>
         <div style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-end'}}>
           <Button onClick={this.goToday}>Today</Button>
-          <DatePicker style={{margin: '12px 0'}} onChange={this.onChange} picker="week" />
+          <DatePicker style={{margin: '12px 0'}} onChange={this.onChange} picker="week" value={selectedDate} />
         </div>
         <div style={{backgroundColor: '#ececec', padding: '30px'}}>
           <Row justify='space-around'>
             {
               R.map((item)=>{
+                const titleStyle : React.CSSProperties = {
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  color: item.color
+                }
                 return (
                   <Col span={3} key={item.title}>
-                    <Card title={item.title} bordered={false} headStyle={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                    <Card title={this.makeTitleNode(item)} bordered={false} headStyle={titleStyle}>
                       Card content
                     </Card>
                   </Col>    
                 );
-              }, data)
+              }, weekData)
             }
           </Row>
         </div>
--- config/constant.ts
+++ config/constant.ts
@@ -3,6 +3,42 @@ import { CalendarOutlined, ScheduleOutlined } from '@ant-design/icons';
 export const HEADER_MENU : string[] = ['calendar', 'setting', 'statistics'];
 export const CALENDAR_SIDE_MENU : string[] = ['Month', 'Week'];
 
+export interface weekTitleObj {
+  title : string,
+  color : string
+}
+
+export const WEEK_TITLE : weekTitleObj[] = [
+  {
+    title : 'Monday',
+    color : '#000000'
+  },
+  {
+    title : 'Tuesday',
+    color : '#000000'
+  },
+  {
+    title : 'Wednesday',
+    color : '#000000'
+  },
+  {
+    title : 'Thursday',
+    color : '#000000'
+  },
+  {
+    title : 'Friday',
+    color : '#000000'
+  },
+  {
+    title : 'Saturday',
+    color : '#0000FF'
+  },
+  {
+    title : 'Sunday',
+    color : '#FF0000'
+  },
+];
+
 // export const CALENDAR_SIDE_MENU : Array<{ name: string, icon: any }> = Array(
 //   {
 //     name : 'Month',
